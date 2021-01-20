00d6356fef370e848fb29fd6294a43fb02e5b778
Marshall FRY <marshalfry1998@gmail.com>
2021-01-20 14:53:55 +0100

    Initialisation du projet + creation de la classe Voiture

---
+++ vuejs_version/src/classes/Voiture.js
@@ -0,0 +1,180 @@
+class Voiture
+{
+
+    constructor(name, description, img, marque, annee_production, 
+        production, classe, usine_assemblage, energie, moteur, position_moteur, cylindree, puissance_maximale, 
+        couple_maximal, transmission, boite_de_vitesses, poids_a_vide,vitesse_maximale,acceleration,
+        consommation_mixte,emission_de_co2,carrosserie,longueur,largeur,hauteur,empattement,coefficient_de_trainee,
+        chassis,plate_forme,suspensions,direction,freins
+        ) {
+        this.name = name
+        this.description = description;
+        this.img = img;
+        this.marque = marque;
+        this.annee_production = annee_production;
+        this.production = production;
+        this.classe = classe;
+        this.usine_assemblage = usine_assemblage;
+        this.energie = energie;
+        this.moteur = moteur;
+        this.position_moteur = position_moteur;
+        this.cylindree = cylindree;
+        this.puissance_maximale = puissance_maximale;
+        this.couple_maximal = couple_maximal;
+        this.transmission = transmission;
+        this.boite_de_vitesses = boite_de_vitesses;
+        this.poids_a_vide = poids_a_vide;
+        this.vitesse_maximale = vitesse_maximale;
+        this.acceleration = acceleration;
+        this.consommation_mixte = consommation_mixte;
+        this.emission_de_co2 = emission_de_co2;
+        this.carrosserie = carrosserie;
+        this.longueur = longueur
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.empattement = empattement;
+        this.coefficient_de_trainee = coefficient_de_trainee;
+        this.chassis = chassis;
+        this.plate_forme = plate_forme;
+        this.suspensions = suspensions;
+        this.direction = direction;
+        this.freins = freins;
+      }
+    
+      get name() {
+        return this.name;
+      }
+
+      get description() {
+        return this.description;
+      }
+
+      get img() {
+        return this.img;
+      }
+
+      get marque() {
+        return this.marque;
+      }
+
+      get annee_production() {
+        return this.annee_production;
+      }
+
+      get production() {
+        return this.production;
+      }
+
+      get classe() {
+        return this.classe;
+      }
+
+      get usine_assemblage() {
+        return this.usine_assemblage;
+      }
+
+      get energie() {
+        return this.energie;
+      }
+
+      get moteur() {
+        return this.moteur;
+      }
+
+      get position_moteur() {
+        return this.position_moteur;
+      }
+
+      get cylindree() {
+        return this.cylindree;
+      }
+
+      get puissance_maximale() {
+        return this.puissance_maximale;
+      }
+
+      get couple_maximal() {
+        return this.couple_maximal;
+      }
+
+      get transmission() {
+        return this.transmission;
+      }
+
+      get boite_de_vitesses() {
+        return this.boite_de_vitesses;
+      }
+
+      get poids_a_vide() {
+        return this.poids_a_vide;
+      }
+
+      get vitesse_maximale() {
+        return this.vitesse_maximale;
+      }
+
+
+      get acceleration() {
+        return this.acceleration;
+      }
+
+      get consommation_mixte() {
+        return this.consommation_mixte;
+      }
+
+      get emission_de_co2() {
+        return this.emission_de_co2;
+      }
+
+
+      get carrosserie() {
+        return this.carrosserie;
+      }
+
+      get longueur() {
+        return this.longueur;
+      }
+
+      get largeur() {
+        return this.largeur;
+      }
+
+
+      get hauteur() {
+        return this.hauteur;
+      }
+
+      get empattement() {
+        return this.empattement;
+      }
+
+      get coefficient_de_trainee() {
+        return this.coefficient_de_trainee;
+      }
+
+
+      get chassis() {
+        return this.chassis;
+      }
+
+
+      get plate_forme() {
+        return this.plate_forme;
+      }
+
+      get suspensions() {
+        return this.suspensions;
+      }
+
+      get direction() {
+        return this.direction;
+      }
+
+      get freins() {
+        return this.freins;
+      }
+
+    
+      
+
+}
